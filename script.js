b813446049aa0f98d0183d896c2956e6eaee4125
derus15 <9608212661i@gmail.com>
2023-12-11 13:58:39 +0400

    added new color + fix bug with negative numbers + rename variables

--- script.js
+++ script.js
@@ -2,34 +2,32 @@ const canvas = document.getElementById('stars');
 const ctx = canvas.getContext('2d');
 
 const stars = [];
-const back = document.getElementById('stars');
-const starsInput = document.querySelector('.params');
-const button = document.querySelector('.button');
+const countStarsInput = document.querySelector('.params');
+const generateButton = document.getElementById('generate');
 const colorStarsInput = document.getElementById('colorStars');
 const colorBackInput = document.getElementById('colorBack');
 const sizeInput = document.getElementById('size');
-let countStars;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 function initStars() {
 
-    countStars = starsInput.value || 200;
-
-    for (let i = 0; i < countStars; i++) {
+    for (let i = 0; i < Math.abs(countStarsInput.value); i++) {
         stars.push({
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
-            size: Math.random() * sizeInput.value,
+            size: Math.random() * Math.abs(sizeInput.value),
             speed: Math.random() + 0.1
         });
     }
+
 }
 
 function drawStars() {
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    back.style.background = colorBackInput.value;
+    canvas.style.background = colorBackInput.value;
 
     for (let i = 0; i < stars.length; i++) {
         const star = stars[i];
@@ -51,12 +49,12 @@ function drawStars() {
 initStars();
 drawStars();
 
-button.addEventListener('click', () => { initStars();} )
+generateButton.addEventListener('click', () => { initStars();} )
 document.addEventListener('keydown', (event) => {
     if (event.keyCode == '32') {
-        starsInput.classList.toggle('hidden');
+        countStarsInput.classList.toggle('hidden');
         colorStarsInput.classList.toggle('hidden');
-        button.classList.toggle('hidden');
+        generateButton.classList.toggle('hidden');
         colorBackInput.classList.toggle('hidden');
         sizeInput.classList.toggle('hidden');
     }
